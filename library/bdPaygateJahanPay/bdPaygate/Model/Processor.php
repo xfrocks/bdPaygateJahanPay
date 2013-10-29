aac48f4c6b3bcdb844b10f3d73d5ec6ac4dd4347
Dao Hoang Son <daohoangson@gmail.com>
2013-10-29 19:22:00 +0700

    v0.9.1 -- added support for bdPaygate_Model_Processor::getCurrencies()

--- library/bdPaygateJahanPay/bdPaygate/Model/Processor.php
+++ library/bdPaygateJahanPay/bdPaygate/Model/Processor.php
@@ -2,6 +2,19 @@
 
 class bdPaygateJahanPay_bdPaygate_Model_Processor extends XFCP_bdPaygateJahanPay_bdPaygate_Model_Processor
 {
+	public function getCurrencies()
+	{
+		$currencies = parent::getCurrencies();
+
+		$api = $options->get('bdPaygateJahanPay_api');
+		if (!empty($api))
+		{
+			$currencies[bdPaygateJahanPay_Processor::CURRENCY_TOMAN] = 'Toman';
+		}
+
+		return $currencies;
+	}
+
 	public function getProcessorNames()
 	{
 		$names = parent::getProcessorNames();
